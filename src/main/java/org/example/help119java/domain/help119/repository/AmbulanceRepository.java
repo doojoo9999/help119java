238a078d40fca3b2fc92b091909a2860a2e80521
doojoo9999 <parkbr95@naver.com>
2025-01-19 04:41:02 +0900

    Repository 리팩토링

---
+++ src/main/java/org/example/help119java/domain/help119/repository/AmbulanceRepository.java
@@ -0,0 +1,7 @@
+package org.example.help119java.domain.help119.repository;
+
+import org.example.help119java.domain.help119.model.AmbulanceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AmbulanceRepository extends JpaRepository<AmbulanceEntity, Long> {
+}
