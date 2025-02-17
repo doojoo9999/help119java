e31bfcf3c864ead231640379d408085e617e8d20
doojoo9999 <parkbr95@naver.com>
2025-02-17 19:48:30 +0900

    Service Kotlin -> Java 리팩토링

--- src/main/java/org/example/help119java/domain/help119/dto/response/AmbulanceResponse.java
+++ src/main/java/org/example/help119java/domain/help119/dto/response/AmbulanceResponse.java
@@ -1,16 +1,14 @@
-package org.example.help119java.domain.help119.dto.request;
-
-import org.example.help119java.domain.help119.model.AmbulanceEntity;
+package org.example.help119java.domain.help119.dto.response;
 
 import java.time.LocalDateTime;
 
-public class AmbulanceRequest {
+public class AmbulanceResponse {
 
     private final String status;
     private final String area;
     private final LocalDateTime time;
 
-    public AmbulanceRequest(String status, String area, LocalDateTime time) {
+    public AmbulanceResponse(String status, String area, LocalDateTime time) {
         this.status = status;
         this.area = area;
         this.time = time;
@@ -26,9 +24,5 @@ public class AmbulanceRequest {
 
     public LocalDateTime getTime() {
         return time;
-    }
-
-    public AmbulanceEntity toAmbulanceEntity() {
-        return new AmbulanceEntity(status, area, time);
     }
 }
--- src/main/java/org/example/help119java/domain/help119/service/SystemService.java
+++ src/main/java/org/example/help119java/domain/help119/service/SystemService.java
@@ -1,28 +1,48 @@
 package org.example.help119java.domain.help119.service;
 
 import org.example.help119java.domain.help119.dto.request.AmbulanceRequest;
+import org.example.help119java.domain.help119.dto.response.AmbulanceResponse;
 import org.example.help119java.domain.help119.model.AmbulanceEntity;
 import org.example.help119java.domain.help119.repository.AmbulanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SystemService {
 
     private final AmbulanceRepository ambulanceRepository;
 
-    @Autowired
     public SystemService(AmbulanceRepository ambulanceRepository) {
         this.ambulanceRepository = ambulanceRepository;
     }
 
-    public void callBy119(AmbulanceRequest ambulanceRequest){
-        AmbulanceEntity ambulanceEntity = new AmbulanceEntity(
+    public void callBy119(AmbulanceRequest ambulanceRequest) {
+        ambulanceRepository.save(new AmbulanceEntity(
                 ambulanceRequest.getStatus(),
                 ambulanceRequest.getArea(),
-                ambulanceRequest.time()
+                ambulanceRequest.getTime()
+        ));
+    }
+
+    public AmbulanceResponse getCalledAmb(Long ambId) {
+        Optional<AmbulanceEntity> ambOptional = ambulanceRepository.findById(ambId);
+        AmbulanceEntity amb = ambOptional.orElseThrow(() -> new IllegalStateException("Amb Not Found"));
+
+        return new AmbulanceResponse(
+                amb.getStatus(),
+                amb.getArea(),
+                amb.getTime()
         );
-        ambulanceRepository.save(ambulanceEntity);
+    }
+
+    public List<AmbulanceResponse> getCalledAmbList() {
+        return ambulanceRepository.findAll().stream()
+                .map(amb -> new AmbulanceResponse(amb.getStatus(), amb.getArea(), amb.getTime()))
+                .collect(Collectors.toList());
     }
 
 }
